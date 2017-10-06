1785ddbf00cc513681014b3b066eb189964adc34
reedsginger <naxicon@gmail.com>
2017-10-05 21:13:41 -0400

    add more special case filtering, a better possesive noun filter, a phrasal function, a possessive filter, and a parenthetical filter

--- searcher.rb
+++ searcher.rb
@@ -1,20 +1,80 @@
-sentences = File.read("VP-Ellipsis-Examples-Thousands copy.txt")
-sentences = sentences.split("\r")
+#! /usr/bin/ruby
 
-#sentences=sentences.map{|s| s.downcase}
-#sentences_filtered = filter(lambda s: "might." in s, sentences)
+def parse (file)
+  sentences = File.read(file)
+  sentences = sentences.split("\r")
 
-sentences_filtered = sentences.select{|s| s.downcase.include?("might.")}
-puts sentences_filtered.length
-sentences_filtered = sentences_filtered.select{|s| !s.downcase.include?("military might.")}
-puts sentences_filtered.length
-sentences_filtered = sentences_filtered.select{|s| !s.downcase.include?("economic might.")}
-puts sentences_filtered.length
-sentences_filtered = sentences_filtered.select{|s| !s.downcase.include?("our might.")}
-puts sentences_filtered.length
-sentences_filtered = sentences_filtered.select{|s| !s.downcase.include?("its might.")}
-puts sentences_filtered.length
+  puts sentences.length
+  sentences
+end
 
-file = sentences_filtered.join("\n\n")
+def might_inclusion(sentences, punctuation)
+  sentences_filtered = Array.new
+  punctuation.each do |p|
+    sentences_filtered += sentences.select{|s| s.downcase.include?("might#{p}")}
+  end
 
-File.write('might.txt',file)
+  puts sentences_filtered.length
+  sentences_filtered
+end
+
+def special_case_filter (sentences_filtered)
+  sentences_filtered = sentences_filtered.select{|s| !s.downcase.include?("military might")}
+  sentences_filtered = sentences_filtered.select{|s| !s.downcase.include?("economic might")}
+  sentences_filtered = sentences_filtered.select{|s| !s.downcase.include?("nato might")}
+  sentences_filtered = sentences_filtered.select{|s| !s.downcase.include?("political might")}
+  sentences_filtered = sentences_filtered.select{|s| !s.downcase.include?("government might")}
+  sentences_filtered = sentences_filtered.select{|s| !s.downcase.include?("financial might")}
+  sentences_filtered = sentences_filtered.select{|s| !s.downcase.include?("diplomatic might")}
+  sentences_filtered = sentences_filtered.select{|s| !s.downcase.include?("industrial might")}
+
+  puts sentences_filtered.length
+  sentences_filtered
+end
+
+def possessive_pronoun_filter (sentences_filtered, possessive_pronouns)
+  possessive_pronouns.each {|pp| sentences_filtered = sentences_filtered.select{|s| !s.downcase.include?("#{pp} might")}}
+
+  puts sentences_filtered.length
+  sentences_filtered
+end
+
+def phrasal_filter(sentences_filtered)
+  sentences_filtered = sentences_filtered.select{|s| !/try as \w* might/.match(s.downcase)}
+
+  puts sentences_filtered.length
+  sentences_filtered
+end
+
+def possessives_filter(sentences_filtered)
+  sentences_filtered = sentences_filtered.select{|s| !/\'s(\s\w*\s|\s)might[^\s]/.match(s.downcase)}
+
+  puts sentences_filtered.length
+  sentences_filtered
+end
+
+def parenthetical_filter (sentences_filtered)
+  capture = sentences_filtered.select{|s|  /might,[\w\s]*\./.match(s.downcase)}
+  sentences_filtered = sentences_filtered - capture
+  sentences_filtered = sentences_filtered.select{|s| !/might,[\w\s]*,[^"|^'']/.match(s.downcase)}
+  sentences_filtered = sentences_filtered + capture
+
+  puts sentences_filtered.length
+  sentences_filtered
+end
+
+file = "VP-Ellipsis-Examples-Thousands copy.txt"
+punctuation = [",", ".", "?", "!", ";", ":"]
+possessive_pronouns = ["my", "your", "his", "her", "its", "our", "their", "one's"]
+
+sentences = parse(file)
+sentences_filtered = might_inclusion(sentences, punctuation)
+sentences_filtered = special_case_filter(sentences_filtered)
+sentences_filtered = possessive_pronoun_filter(sentences_filtered, possessive_pronouns)
+sentences_filtered = phrasal_filter(sentences_filtered)
+sentences_filtered = possessives_filter(sentences_filtered)
+sentences_filtered = parenthetical_filter(sentences_filtered)
+
+output = sentences_filtered.join("\n\n")
+
+File.write('might.txt',output)
